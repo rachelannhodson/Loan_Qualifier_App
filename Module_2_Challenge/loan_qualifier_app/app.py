022830f93b7bc1a9c67960a89d9224fca663d6c9
Rachel Ann Hodson <rachelannhodson@gmail.com>
2022-10-30 19:30:24 -0700

    <Making progress on the save_qualifying_loans function, commiting to not lose my progress>

--- Module_2_Challenge/loan_qualifier_app/app.py
+++ Module_2_Challenge/loan_qualifier_app/app.py
@@ -104,8 +104,6 @@ def find_qualifying_loans(bank_data, credit_score, debt, income, loan, home_valu
 
     return bank_data_filtered
 
-# add a function which can save a new csv file with only the loans a potential borrower is qualified for
-save_csv = []
 
 def save_qualifying_loans(qualifying_loans):
     """Saves the qualifying loans to a CSV file.
@@ -114,22 +112,11 @@ def save_qualifying_loans(qualifying_loans):
         qualifying_loans (list of lists): The qualifying bank loans.
     """
     # @TODO: Complete the usability dialog for savings the CSV Files.
+    save_qualifying_loans = questionary.confirm("Would you like to save this list of qualifying loans to a CSV file?").ask()
+    save_qualifying_loans = questionary.text("Enter a file path (.csv):").ask()
+    save_csv(save_qualifying_loans)
+
     
-# add a function which can save a new csv file with only the loans a potential borrower is qualified for
-save_csv = []
-# add a header for the csv file
-header = ["Lender", "Max Loan Amount", "Max LTV", "Max DTI", "Min Credit Score", "Interest Rate"]
-# designate a path for the file to populate on the local computer
-output_path = Path("save_csv.csv")
-# function to write a new csv file with only the loans a potential borrower is qualified for
-with open(output_path, 'w', newline='') as csvfile:
-    csvwriter = csv.writer(csvfile, delimiter=",")
-    csvwriter.writerow(header)
-    for loan in save_csv:
-        csvwriter.writerow(loan.values())
-        print(loan.values())
-
-
 def run():
     """The main function for running the script."""
 
--- Module_2_Challenge/loan_qualifier_app/qualifier/utils/save_csv.py
+++ Module_2_Challenge/loan_qualifier_app/qualifier/utils/save_csv.py
@@ -2,19 +2,18 @@ import csv
 from pathlib import Path
 
 
-
+csvpath = ''
 # add a header for the csv file
 header = ["Lender", "Max Loan Amount", "Max LTV", "Max DTI", "Min Credit Score", "Interest Rate"]
 # designate a path for the file to populate on the local computer
-output_path = Path("save_csv.csv")
 
 def save_csv(save_qualifying_loans):
- 
+    save_cvs = [] 
     # function to write a new csv file with only the loans a potential borrower is qualified for
-    with open(output_path, 'w', newline='') as csvfile:
+    with open(csvpath, 'w', newline='') as csvfile:
         csvwriter = csv.writer(csvfile, delimiter=",")
         csvwriter.writerow(header)
-        for loan in save_csv:
-            csvwriter.writerow(loan.values())
-            print(loan.values())
+        for row in csvwriter:
+            csvwriter.writerow(row.values())
+            print(row.values())
     
